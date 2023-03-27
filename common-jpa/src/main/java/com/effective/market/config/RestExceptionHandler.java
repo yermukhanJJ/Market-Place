e0a130956bc92f8d5b54a97fdf801130db2339a9
yermukhanJJ <ermuhannurkali@gmail.com>
2023-03-27 16:48:09 +0600

    Refactoring: fix exception

---
+++ common-jpa/src/main/java/com/effective/market/config/RestExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.effective.market.config;
+
+import com.effective.market.dto.ErrorResponse;
+import com.effective.market.exception.BadRequestException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class RestExceptionHandler {
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<ErrorResponse> handlerBadRequestException(BadRequestException e) {
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), e.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+}
---
+++ common-jpa/src/main/java/com/effective/market/dto/ErrorResponse.java
@@ -0,0 +1,16 @@
+package com.effective.market.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ErrorResponse {
+
+    private int errorCode;
+    private String message;
+}
